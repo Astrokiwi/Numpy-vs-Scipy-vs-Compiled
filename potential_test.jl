7d64c1cf2b6e58a3b04810c0e83ed1853af99de9
David J Williamson <davidjwilliamson@davids-mbp.clients.soton.ac.uk>
2019-08-05 09:25:49 +0100

    Fixed accidental global in julia code

--- potential_test.jl
+++ potential_test.jl
@@ -1,13 +1,13 @@
 function softened_potential_threeloop(r,h)
     # simple unwrapped loop
-    n = size(r)
+    N = size(r)[1]
     pot = 0.
     h2 = h^2
-    for i=1:N-1
+    @inbounds for i=1:N-1
         for j=i+1:N
             d=0.
             for k=1:3
-                d+=(r[i,k]-r[j,k]).^2
+                @views d+=(r[i,k]-r[j,k]).^2
             end
             d+=h2
             d = sqrt(d)^-1
@@ -19,12 +19,12 @@ end
 
 function softened_potential_twoloop(r,h)
     # simple unwrapped loop
-    n = size(r)
+    N = size(r)[1]
     pot = 0.
     h2 = h^2
-    for i=1:N-1
+    @inbounds for i=1:N-1
         for j=i+1:N
-            pot+=sqrt((sum((r[i,:].-r[j,:]).^2).+h2))^-1
+            @views pot+=sqrt((sum((r[i,:].-r[j,:]).^2).+h2)).^-1
         end
     end
     return pot
@@ -32,11 +32,11 @@ end
 
 function softened_potential_oneloop(r,h)
     # vector operations
-    n = size(r)
+    n = size(r)[1]
     pot = 0.
     h2 = h^2
-    for i=1:N-1
-        pot+=sum(sqrt.((sum((r[i,:]'.-r[i+1:N,:]).^2,dims=2).+h2)).^-1)
+    @inbounds for i=1:N-1
+        @views pot+=sum(sqrt.((sum((r[i,:]'.-r[i+1:N,:]).^2,dims=2).+h2)).^-1)
     end
     return pot
 end
@@ -44,9 +44,9 @@ end
 
 using Random
 
-const N = parse(Int64,ARGS[1])
-const r = Random.rand(N,3)
-const h = 0.01
+N = parse(Int64,ARGS[1])
+r = Random.rand(N,3)
+h = 0.01
 
 # run twice, so timing isn't dominated by compile time
 for i=1:2
